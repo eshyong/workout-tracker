f2fe8fd543370af0a8eabe69c1210c1c66010b44
eshyong <eric.shyong@gmail.com>
2016-03-11 17:34:42 -0800

    Simplified enter-workout page and logic

--- public/javascript/enterWorkout.js
+++ public/javascript/enterWorkout.js
@@ -3,116 +3,78 @@
 $(document).ready(() => {
   console.log('ready!');
 
-  $('.workout-selector[name="button-a"]').click((event) => {
-    console.log('Workout A selected');
-    $('#form-a').fadeIn('slow');
-    $('#form-b').hide();
-  });
-
-  $('.workout-selector[name="button-b"]').click(() => {
-    console.log('Workout B selected');
-    $('#form-b').fadeIn('slow');
-    $('#form-a').hide();
-  });
-
   // Error function for form submission
-  var showError = (fieldName) => {
-    $('#failure').text('Please enter a positive number in the "' + fieldName + '" field.')
+  var showError = (message) => {
+    $('#failure').text(message)
                  .fadeIn()
                  .delay(2000)
                  .fadeOut();
   }
 
-  $('#form-a').submit((event) => {
-    console.log('Submitting workout A');
+  // Set the default value for the date input to be today.
+  $('#date').val(moment().format('YYYY-MM-DD'));
 
-    // Get input numbers from form
-    var target = event.target;
-    var squats = Number.parseInt($(target).find('#squats-a').val());
-    var benchPress = Number.parseInt($(target).find('#bench-press').val());
-    var overheadRows = Number.parseInt($(target).find('#overhead-rows').val());
+  $('#workout-form').submit((event) => {
+    console.log('Submitting workout');
+
+    // Get input numbers for each exercise, as well as the date of the workout, from the form.
+    var target = event.target,
+        squats = Number.parseInt($(target).find('#squats').val()),
+        benchPress = Number.parseInt($(target).find('#bench-press').val()),
+        barbellRows = Number.parseInt($(target).find('#barbell-rows').val()),
+        overheadPress = Number.parseInt($(target).find('#overhead-press').val()),
+        deadlifts = Number.parseInt($(target).find('#deadlifts').val()),
+        date = $(target).find('#date').val() || moment().format('YYYY-MM-DD');
     console.log('Squats: ' + squats + ', Bench Press: ' + benchPress +
-                ', Overhead Rows: ' + overheadRows);
-
-    // Confirm that inputs are positive integers
-    if (isNaN(squats) || squats < 0) {
-      showError('Squats');
-      event.preventDefault();
-      return;
-    }
-
-    if (isNaN(benchPress) || benchPress < 0) {
-      showError('Bench Press');
-      event.preventDefault();
-      return;
-    }
-
-    if (isNaN(overheadRows) || overheadRows < 0) {
-      showError('Overhead Rows');
-      event.preventDefault();
-      return;
-    }
-
-    // Send information to server
-    var data = JSON.stringify({
-      workoutType: 'A', 
-      squats: squats,
-      benchPress: benchPress,
-      overheadRows: overheadRows
-    });
-    var settings = {
-      contentType: 'application/json',
-      data: data,
-      dataType: 'json',
-      url: '/enter-workout'
-    };
-    $.post(settings).done((response) => {
-      if (response['status'] === 'success') {
-        $('#success').text('Successfully entered workout! ').fadeIn();
-        $('#success').append('<a href="workouts">View submitted workouts</a.');
-      } else if (response['status'] === 'failure') {
-        $('#failure').text('Sorry, we failed to process your workout. Reason: ' + response['message']);
-      }
-    });
-    event.preventDefault();
-  });
-  
-  $('#form-b').submit((event) => {
-    console.log('Submitting workout B');
-
-    // Get input numbers from form
-    var target = event.target;
-    var squats = Number.parseInt($(target).find('#squats-b').val());
-    var overheadPress = Number.parseInt($(target).find('#overhead-press').val());
-    var deadlifts = Number.parseInt($(target).find('#deadlifts').val());
-    console.log('Squats: ' + squats + ', Overhead Press: ' + overheadPress +
+                ', Barbell Rows: ' + barbellRows +
+                ', Overhead Press: ' + overheadPress +
                 ', Deadlifts: ' + deadlifts);
 
     // Confirm that inputs are positive integers
-    if (isNaN(squats) || squats < 0) {
-      showError('Squats');
+    var validateIntegerOrShowError = (exerciseWeight, exerciseName) => {
+      if (isNaN(exerciseWeight) || exerciseWeight < 0) {
+        showError('Please enter a positive number in the "' + exerciseName + '" field.');
+        return false;
+      }
+      return true;
+    }
+
+    if (!validateIntegerOrShowError(squats, 'Squats')) {
+      event.preventDefault();
+      return;
+    }
+    if (!validateIntegerOrShowError(benchPress, 'Bench Press')) {
+      event.preventDefault();
+      return;
+    }
+    if (!validateIntegerOrShowError(barbellRows, 'Barbell Rows')) {
+      event.preventDefault();
+      return;
+    }
+    if (!validateIntegerOrShowError(overheadPress, 'Overhead Press')) {
+      event.preventDefault();
+      return;
+    }
+    if (!validateIntegerOrShowError(deadlifts, 'Deadlifts')) {
       event.preventDefault();
       return;
     }
 
-    if (isNaN(overheadPress) || overheadPress < 0) {
-      showError('Overhead Press');
-      event.preventDefault();
-      return;
-    }
-
-    if (isNaN(deadlifts) || deadlifts < 0) {
-      showError('Deadlifts');
+    if (squats === 0 && benchPress === 0 && barbellRows === 0 &&
+        overheadPress === 0 && deadlifts === 0) {
+      showError('Please enter at least one non-zero exercise weight.');
       event.preventDefault();
       return;
     }
 
     // Send information to server
     var data = JSON.stringify({
-      workoutType: 'B', 
       squats: squats,
-      overheadPress: overheadPress,
-      deadlifts: deadlifts
+      bench_press: benchPress,
+      barbell_rows: barbellRows,
+      overhead_press: overheadPress,
+      deadlifts: deadlifts,
+      date: date
     });
     var settings = {
       contentType: 'application/json',
@@ -122,10 +84,13 @@ $(document).ready(() => {
     };
     $.post(settings).done((response) => {
       if (response['status'] === 'success') {
-        $('#success').text('Successfully entered workout! ').fadeIn();
-        $('#success').append('<a href="workouts">View submitted workouts</a.');
+        $('#success').text('Successfully entered workout! ');
+        $('#success').append('<a href="workouts">View submitted workouts</a>.');
+        $('#success').fadeIn().delay(5000).fadeOut();
       } else if (response['status'] === 'failure') {
-        $('#failure').text('Sorry, we failed to process your workout. Reason: ' + response['message']);
+        $('#failure').text('Sorry, we failed to process your workout. Reason: ' +
+                           response['message']);
+        $('#failure').fadeIn().delay(5000).fadeOut();
       }
     });
     event.preventDefault();
