4fa2af822747f75fe39289edaaedada93195cdd9
eshyong <eric.shyong@gmail.com>
2016-03-12 21:17:56 -0800

    JsFormat

--- public/javascript/enterWorkout.js
+++ public/javascript/enterWorkout.js
@@ -6,9 +6,9 @@ $(document).ready(() => {
   // Error function for form submission
   var showError = (message) => {
     $('#failure').text(message)
-                 .fadeIn()
-                 .delay(2000)
-                 .fadeOut();
+      .fadeIn()
+      .delay(2000)
+      .fadeOut();
   }
 
   // Set the default value for the date input to be today.
@@ -20,16 +20,16 @@ $(document).ready(() => {
 
     // Get input numbers for each exercise, as well as the date of the workout, from the form.
     let target = event.target,
-        squats = Number.parseInt($(target).find('#squats').val()),
-        benchPress = Number.parseInt($(target).find('#bench-press').val()),
-        barbellRows = Number.parseInt($(target).find('#barbell-rows').val()),
-        overheadPress = Number.parseInt($(target).find('#overhead-press').val()),
-        deadlifts = Number.parseInt($(target).find('#deadlifts').val()),
-        workoutDate = $(target).find('#date').val() || moment().format('YYYY-MM-DD');
+      squats = Number.parseInt($(target).find('#squats').val()),
+      benchPress = Number.parseInt($(target).find('#bench-press').val()),
+      barbellRows = Number.parseInt($(target).find('#barbell-rows').val()),
+      overheadPress = Number.parseInt($(target).find('#overhead-press').val()),
+      deadlifts = Number.parseInt($(target).find('#deadlifts').val()),
+      workoutDate = $(target).find('#date').val() || moment().format('YYYY-MM-DD');
     console.log('Squats: ' + squats + ', Bench Press: ' + benchPress +
-                ', Barbell Rows: ' + barbellRows +
-                ', Overhead Press: ' + overheadPress +
-                ', Deadlifts: ' + deadlifts);
+      ', Barbell Rows: ' + barbellRows +
+      ', Overhead Press: ' + overheadPress +
+      ', Deadlifts: ' + deadlifts);
 
     // Confirm that inputs are positive integers
     var validateIntegerOrShowError = (exerciseWeight, exerciseName) => {
@@ -58,7 +58,7 @@ $(document).ready(() => {
 
     // At least one exercise should be non-zero
     if (squats === 0 && benchPress === 0 && barbellRows === 0 &&
-        overheadPress === 0 && deadlifts === 0) {
+      overheadPress === 0 && deadlifts === 0) {
       showError('Please enter at least one non-zero exercise weight.');
       return;
     }
@@ -86,13 +86,17 @@ $(document).ready(() => {
     };
     $.post(settings).done((response) => {
       if (response['status'] === 'success') {
-        $('#success').text('Successfully entered workout! ');
-        $('#success').append('<a href="workouts">View submitted workouts</a>.');
-        $('#success').fadeIn().delay(5000).fadeOut();
+        $('#success').text('Successfully entered workout! ')
+          .append('<a href="workouts">View submitted workouts</a>.')
+          .fadeIn()
+          .delay(5000)
+          .fadeOut();
       } else if (response['status'] === 'failure') {
         $('#failure').text('Sorry, we failed to process your workout. Reason: ' +
-                           response['message']);
-        $('#failure').fadeIn().delay(5000).fadeOut();
+            response['message'])
+          .fadeIn()
+          .delay(5000)
+          .fadeOut();
       }
     });
   });
