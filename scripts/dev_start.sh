6069ba153042622be0c574f8262dff990f2e0dea
eshyong <eric.shyong@gmail.com>
2016-03-19 12:33:36 -0700

    Add `npm start` and `npm stop` commands

--- scripts/dev_start.sh
+++ scripts/dev_start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Turn on the following flags
+# -e: terminate script on error
+# -u: terminate script on unbound variable
+# -x: log each line of the script as it is executed
+# -o pipefail: sets the exit status of a pipe to the last
+# 	command that returned a non-zero status
 set -eux -o pipefail
 
-# OSX
-if [[ "$(uname)" = "Darwin" ]]; then
-	mysql.server start
-fi
-
+# Start MySQL server and Node server
+mysql.server start
 nodemon
--- scripts/dev_stop.sh
+++ scripts/dev_stop.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Turn on the following flags:
+# -e: terminate script on error
+# -u: terminate script on unbound variable
+# -x: log each line of the script as it is executed
+# -o pipefail: sets the exit status of a pipe to the last
+# 	command that returned a non-zero status
 set -eux -o pipefail
 
-# Kill node processes
+# Kill any node processes, including nodemon
+# Then shut off MySQL
 pkill node
-
-# OSX
-if [[ "$(uname)" = "Darwin" ]]; then
-    mysql.server stop
-fi
+mysql.server stop
