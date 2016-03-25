37389738f77e991e32be019ed151d047624bc521
eshyong <eric.shyong@gmail.com>
2016-03-25 15:20:58 -0700

    Remove sudo invocations and add node symlink

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -6,12 +6,15 @@ set -eux -o pipefail
 source ./env_vars
 
 # Update apt repos
-sudo apt-get update
+apt-get update
 
 # Install git, nodejs, npm, and mysql
-sudo apt-get --yes install git
-sudo apt-get --yes install nodejs
-sudo apt-get --yes install npm
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_ROOT_PW'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW'
-sudo apt-get --yes install mysql-server
+apt-get --yes install git
+apt-get --yes install nodejs
+apt-get --yes install npm
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PW"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW"
+apt-get --yes install mysql-server
+
+# Alias "node" to "nodejs"
+ln -s /usr/bin/nodejs /usr/local/bin/node
