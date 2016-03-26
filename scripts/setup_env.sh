6b04a7e1e398818e0e82ed1347b734ac0dcc70ca
eshyong <eric.shyong@gmail.com>
2016-03-26 00:56:20 -0700

    Upgrade node and npm

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -17,10 +17,18 @@ apt-get update
 # Install git, nodejs, and npm
 apt-get --yes install git nodejs npm
 
+# Upgrade npm and node
+npm install --global n
+n 5.6.0
+
+# Alias node and npm to n's latest versions
+ln -sf /usr/local/n/versions/node/5.6.0/bin/npm /usr/local/bin/npm
+ln -sf /usr/local/n/versions/node/5.6.0/bin/node /usr/local/bin/node
+
 # Install MySQL 5.6
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PW"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW"
 apt-get --yes install mysql-server-5.6
 
-# Alias "node" to "nodejs"
-ln -sf /usr/bin/nodejs /usr/local/bin/node
+# Install necessary packages
+npm install
