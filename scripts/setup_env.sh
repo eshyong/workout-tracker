98c5d3caab525e139a4d29563187a335e19e7f9f
Eric Shyong <eric.shyong@gmail.com>
2016-03-26 02:09:37 +0000

    Had to increase VM memory to install mysql 5.6

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -8,13 +8,13 @@ source ./env_vars
 # Update apt repos
 apt-get update
 
-# Install git, nodejs, npm, and mysql
-apt-get --yes install git
-apt-get --yes install nodejs
-apt-get --yes install npm
+# Install git, nodejs, and npm
+apt-get --yes install git nodejs npm
+
+# Install MySQL 5.6
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PW"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW"
-apt-get --yes install mysql-server
+apt-get --yes install mysql-server-5.6
 
 # Alias "node" to "nodejs"
-ln -s /usr/bin/nodejs /usr/local/bin/node
+ln -sf /usr/bin/nodejs /usr/local/bin/node
