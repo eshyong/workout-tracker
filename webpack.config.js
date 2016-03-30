a22ac5ef5a4edb6d303d313bc2e878f594e3035a
eshyong <eric.shyong@gmail.com>
2016-03-29 20:51:30 -0700

    Another rename

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: {
-    login: './client/Login.jsx',
-    editWorkouts: './client/EditWorkouts.jsx'
+    loginPage: './client/LoginPage.jsx',
+    editWorkoutsPage: './client/EditWorkoutsPage.jsx'
   },
   module: {
     loaders: [{
