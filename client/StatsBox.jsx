93d19c54af2b2285f304f71b91f85bddea729f93
eshyong <eric.shyong@gmail.com>
2016-04-06 01:56:07 -0700

    Print stats with 1 decimal precision

--- client/StatsBox.jsx
+++ client/StatsBox.jsx
@@ -1,46 +1,90 @@
 'use strict';
 
+// External dependencies
 var React = require('react');
+
+// Default precision for floating point numbers
+const defaultPrecision = 1;
 
 var StatsBox = React.createClass({
   getInitialState: function() {
     return {};
   },
   render: function() {
+    // Display null workouts as 0
     return (
       <div className="StatsBox">
-        <h2>Workout stats</h2>
         <h3>Averages</h3>
         <div>Squats: {
-          this.props.averages.squats === null ? '0' : this.props.averages.squats
+          this.props.averages.squats ? (
+            this.props.averages.squats.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Bench press: {
-          this.props.averages.benchPress === null ? '0' : this.props.averages.benchPress
+          this.props.averages.benchPress ? (
+            this.props.averages.benchPress.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Barbell rows: {
-          this.props.averages.barbellRows === null ? '0' : this.props.averages.barbellRows
+          this.props.averages.barbellRows ? (
+            this.props.averages.barbellRows.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Overhead press: {
-          this.props.averages.overheadPress === null ? '0' : this.props.averages.overheadPress
+          this.props.averages.overheadPress ? (
+            this.props.averages.overheadPress.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Deadlifts: {
-          this.props.averages.deadlifts === null ? '0' : this.props.averages.deadlifts
+          this.props.averages.deadlifts ? (
+            this.props.averages.deadlifts.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <h3>Max weights</h3>
         <div>Squats: {
-          this.props.maxes.squats === null ? '0' : this.props.maxes.squats
+          this.props.maxes.squats ? (
+            this.props.maxes.squats.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Bench press: {
-          this.props.maxes.benchPress === null ? '0' : this.props.maxes.benchPress
+          this.props.maxes.benchPress ? (
+            this.props.maxes.benchPress.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Barbell rows: {
-          this.props.maxes.barbellRows === null ? '0' : this.props.maxes.barbellRows
+          this.props.maxes.barbellRows ? (
+            this.props.maxes.barbellRows.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Overhead press: {
-          this.props.maxes.overheadPress === null ? '0' : this.props.maxes.overheadPress
+          this.props.maxes.overheadPress ? (
+            this.props.maxes.overheadPress.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
         <div>Deadlifts: {
-          this.props.maxes.deadlifts === null ? '0' : this.props.maxes.deadlifts
+          this.props.maxes.deadlifts ? (
+            this.props.maxes.deadlifts.toFixed(defaultPrecision)
+          ) : (
+            '0'
+          )
         } </div>
       </div>
     );
