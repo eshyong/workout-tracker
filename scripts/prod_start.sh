3917f7599ec66f7f41def9a49782b9233ceb3155
eshyong <eric.shyong@gmail.com>
2016-04-07 19:03:55 -0700

    Add prod start script

---
+++ scripts/prod_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source environment variables file
+source ./prod_env_vars
+
+# Turn on the following flags
+# -e: terminate script on error
+# -u: terminate script on unbound variable
+# -x: log each line of the script as it is executed
+# -o pipefail: sets the exit status of a pipe to the last
+# 	command that returned a non-zero status
+set -eux -o pipefail
+
+# Start redis server
+if ! pgrep redis-server; then
+    service redis-server start
+fi
+
+webpack --color --progress && node app.js
