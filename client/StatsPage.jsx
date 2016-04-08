2bf8001981fb3756f22313e841d17141e3216961
eshyong <eric.shyong@gmail.com>
2016-04-07 20:41:21 -0700

    Only show workout graphs if more than 3 workouts of each type is entered

--- client/StatsPage.jsx
+++ client/StatsPage.jsx
@@ -64,6 +64,16 @@ var StatsPage = React.createClass({
       top: 50,
       bottom: 50
     };
+    var aWorkoutCount = 0;
+    var bWorkoutCount = 0;
+    this.state.workouts.forEach(function(workout) {
+      if (workout.is_type_a) {
+        aWorkoutCount += 1;
+      } else {
+        bWorkoutCount += 1;
+      }
+    });
+    var showGraphs = (aWorkoutCount >= 3) && (bWorkoutCount >= 3);
     return (
       <div className="StatsPage">
         <Navbar
@@ -99,91 +109,101 @@ var StatsPage = React.createClass({
           maxes={this.state.maxes}
         />
         <h2>Workout graphs</h2>
-        <WorkoutGraph
-          title="Squats"
-          workouts={this.state.workouts}
-          fieldName="squats"
-          chartSeries={
-            [
-              {
-                field: 'squats',
-                name: 'Squats',
-                color: '#FF0000'
-              }
-            ]
-          }
-          width={width}
-          height={height}
-          margins={margins}
-        />
-        <WorkoutGraph
-          title="Bench Press"
-          workouts={this.state.workouts}
-          fieldName="bench_press"
-          chartSeries={
-            [
-              {
-                field: 'bench_press',
-                name: 'Bench Press',
-                color: '#0000FF'
-              }
-            ]
-          }
-          width={width}
-          height={height}
-          margins={margins}
-        />
-        <WorkoutGraph
-          title="Barbell Rows"
-          workouts={this.state.workouts}
-          fieldName="barbell_rows"
-          chartSeries={
-            [
-              {
-                field: 'barbell_rows',
-                name: 'Barbell Rows',
-                color: '#00FF00'
-              }
-            ]
-          }
-          width={width}
-          height={height}
-          margins={margins}
-        />
-        <WorkoutGraph
-          title="Overhead Press"
-          workouts={this.state.workouts}
-          fieldName="overhead_press"
-          chartSeries={
-            [
-              {
-                field: 'overhead_press',
-                name: 'Overhead Press',
-                color: '#FFAA33'
-              }
-            ]
-          }
-          width={width}
-          height={height}
-          margins={margins}
-        />
-        <WorkoutGraph
-          title="Deadlifts"
-          workouts={this.state.workouts}
-          fieldName="deadlifts"
-          chartSeries={
-            [
-              {
-                field: 'deadlifts',
-                name: 'Deadlifts',
-                color: '#00FFFF'
-              }
-            ]
-          }
-          width={width}
-          height={height}
-          margins={margins}
-        />
+        {
+          !showGraphs ? (
+            <div className="notEnoughWorkouts">
+              Please add at least 3 workouts of each type to see your graphs.
+            </div>
+          ) : (
+            <div>
+              <WorkoutGraph
+                title="Squats"
+                workouts={this.state.workouts}
+                fieldName="squats"
+                chartSeries={
+                  [
+                    {
+                      field: 'squats',
+                      name: 'Squats',
+                      color: '#FF0000'
+                    }
+                  ]
+                }
+                width={width}
+                height={height}
+                margins={margins}
+              />
+              <WorkoutGraph
+                title="Bench Press"
+                workouts={this.state.workouts}
+                fieldName="bench_press"
+                chartSeries={
+                  [
+                    {
+                      field: 'bench_press',
+                      name: 'Bench Press',
+                      color: '#0000FF'
+                    }
+                  ]
+                }
+                width={width}
+                height={height}
+                margins={margins}
+              />
+              <WorkoutGraph
+                title="Barbell Rows"
+                workouts={this.state.workouts}
+                fieldName="barbell_rows"
+                chartSeries={
+                  [
+                    {
+                      field: 'barbell_rows',
+                      name: 'Barbell Rows',
+                      color: '#00FF00'
+                    }
+                  ]
+                }
+                width={width}
+                height={height}
+                margins={margins}
+              />
+              <WorkoutGraph
+                title="Overhead Press"
+                workouts={this.state.workouts}
+                fieldName="overhead_press"
+                chartSeries={
+                  [
+                    {
+                      field: 'overhead_press',
+                      name: 'Overhead Press',
+                      color: '#FFAA33'
+                    }
+                  ]
+                }
+                width={width}
+                height={height}
+                margins={margins}
+              />
+              <WorkoutGraph
+                title="Deadlifts"
+                workouts={this.state.workouts}
+                fieldName="deadlifts"
+                chartSeries={
+                  [
+                    {
+                      field: 'deadlifts',
+                      name: 'Deadlifts',
+                      color: '#00FFFF'
+                    }
+                  ]
+                }
+                width={width}
+                height={height}
+                margins={margins}
+              />
+            </div>
+          )
+        }
       </div>
     );
   }
