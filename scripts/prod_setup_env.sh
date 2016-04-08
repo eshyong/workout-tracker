f064964c18edd709cd62f96457dbe4883415babf
eshyong <eric.shyong@gmail.com>
2016-04-07 18:02:58 -0700

    Add public prod setup script

---
+++ scripts/prod_setup_env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Source environment variables file
+source ./prod_env_vars
+
+# Turn on the following flags
+# -e: terminate script on error
+# -u: terminate script on unbound variable
+# -x: log each line of the script as it is executed
+# -o pipefail: sets the exit status of a pipe to the last
+#   command that returned a non-zero status
+set -eux -o pipefail
+
+# Update apt repos
+apt-get update
+
+# Install git, redis-server, and mysql-client
+apt-get --yes install git redis-server mysql-client
+
+# Secure redis server by requiring authentication
+sed -i.bak "s/# requirepass foobared/requirepass $REDIS_PW/g" /etc/redis/redis.conf
+service redis-server restart
+
+# Install node 5.6.0
+pushd /tmp
+curl -O https://nodejs.org/dist/v5.6.0/node-v5.6.0-linux-x64.tar.xz
+pushd /opt
+tar xvf /tmp/node-v5.6.0-linux-x64.tar.xz
+sudo ln -sf /opt/node-v5.6.0-linux-x64/bin/node /usr/local/bin/node
+sudo ln -sf /opt/node-v5.6.0-linux-x64/bin/npm /usr/local/bin/npm
+popd
+popd
+
+# Install nodemon and webpack globally
+npm install --global nodemon webpack
+
+# Cleanup
+rm -rf /tmp/*
